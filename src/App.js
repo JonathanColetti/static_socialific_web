2fb311a40d36fb56c703a5bb3295214b67e58cac
JonathanColetti <88168630+JonathanColetti@users.noreply.github.com>
2022-10-30 20:44:42 -0400

    Update App.js
    Update to more conventional naming

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './index.css'
 import TopBar from './components/TopBar';
-import Middle from './components/Middle';
+import Body from './components/Body';
 import Footer from './components/Footer';
 function App() {
   return (
     <>
       <div style={{ width: '100%', height: '100%', padding: '11%' }} className="App  bg-yellow-100">
         <TopBar />
-        <Middle />
+        <Body />
         <Footer />
 
       </div>
